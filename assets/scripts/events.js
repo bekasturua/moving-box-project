a4db5298c4f0a73fc4ee573351147b2b19ecdcde
Beka Sturua <beqasturua3@gmail.com>
2022-06-12 18:04:30 +0400

    update project

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,38 +1,38 @@
-const button = document.querySelector('button');
-const buttonClickHandler = event => {
+const button = document.querySelector("button");
+const buttonClickHandler = (event) => {
   console.log(event);
 };
 
 const anotherButtonClickHandler = () => {
-  console.log('This was clicked!');
+  console.log("This was clicked!");
 };
 
 const boundFn = buttonClickHandler.bind(this);
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(event);
 });
 
-const div = document.querySelector('div');
+const div = document.querySelector("div");
 
-div.addEventListener('mouseenter', event => {
-  console.log('CLICKED DIV');
+div.addEventListener("mouseenter", (event) => {
+  console.log("CLICKED DIV");
   console.log(event);
 });
 
-button.addEventListener('click', function(event) {
+button.addEventListener("click", function (event) {
   event.stopPropagation();
-  console.log('CLICKED BUTTON');
+  console.log("CLICKED BUTTON");
   console.log(event);
   console.log(this);
 });
 
-const listItems = document.querySelectorAll('li');
-const list = document.querySelector('ul');
-list.addEventListener('click', function(event) {
-  event.target.closest('li').classList.toggle('highlight');
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+list.addEventListener("click", function (event) {
+  event.target.closest("li").classList.toggle("highlight");
   button.click();
   console.log(this);
 });
